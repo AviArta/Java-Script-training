53695e4b1d26308a7f95ce7650603a5345da0be3
AviArta <buzlova.a@yandex.ru>
2023-03-18 20:25:37 +0300

    Corr HW4_Task-1

--- JS_HW-4/hw4_task-1.js
+++ JS_HW-4/hw4_task-1.js
@@ -1,4 +1,17 @@
-function getPasswordChecker(password) {
+// функция, возвращающая в качестве результата фунцию, проверяющую пароль пользователя
+function getPasswordChecker(variantPassword) {
+    rightPassword = 'AKuvshinova_92';
+    return function() {
+        if (variantPassword == rightPassword) {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+// Функция не по заданию, оставила на память)
+function checkValidPassword(password) {
     // от 7 до 14 символов, любой цифро-буквенный символ, вкл подчеркивание
     let rightPassword = /^[A-Za-z]\w{7,15}$/; 
     return function() {
@@ -10,12 +23,12 @@ function getPasswordChecker(password) {
     }
 }
 
-let password1 = getPasswordChecker('fghH_cbf');
-let password2 = getPasswordChecker('1fghHjcbf');
-let password3 = getPasswordChecker('06567489');
-let password4 = getPasswordChecker('?:%;?;*%;*');
+let variantPassword1 = getPasswordChecker('AKuvshinova_92');
+let variantPassword2 = getPasswordChecker('1fghHjcbf');
+let variantPassword3 = getPasswordChecker('06567489');
+let variantPassword4 = getPasswordChecker('AKuvshinova_922');
 
-console.log(password1());
-console.log(password2());
-console.log(password3());
-console.log(password4());
+console.log(variantPassword1());
+console.log(variantPassword2());
+console.log(variantPassword3());
+console.log(variantPassword4());
