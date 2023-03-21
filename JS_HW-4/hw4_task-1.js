95809a16ce64b022028edbecae671921222bf13a
AviArta <buzlova.a@yandex.ru>
2023-03-21 12:01:35 +0300

    Corr task-1

--- JS_HW-4/hw4_task-1.js
+++ JS_HW-4/hw4_task-1.js
@@ -1,34 +1,23 @@
-// функция, возвращающая в качестве результата фунцию, проверяющую пароль пользователя
-function getPasswordChecker(variantPassword) {
-    rightPassword = 'AKuvshinova_92';
-    return function() {
-        if (variantPassword == rightPassword) {
-            return true
-        } else {
-            return false
-        }
+// функция, возвращающая в качестве результата функцию, проверяющую пароль пользователя
+function getPasswordChecker(rightPassword) {
+    return function(variantPassword) {
+        return variantPassword == rightPassword
     }
 }
 
 // Функция не по заданию, оставила на память)
+// Определяет валидность пароля
 function checkValidPassword(password) {
     // от 7 до 14 символов, любой цифро-буквенный символ, вкл подчеркивание
     let rightPassword = /^[A-Za-z]\w{7,15}$/; 
     return function() {
-        if (password.match(rightPassword)) {
-        return true
-        } else {
-            return false
-        }
+        return password.match(rightPassword)
     }
 }
 
-let variantPassword1 = getPasswordChecker('AKuvshinova_92');
-let variantPassword2 = getPasswordChecker('1fghHjcbf');
-let variantPassword3 = getPasswordChecker('06567489');
-let variantPassword4 = getPasswordChecker('AKuvshinova_922');
+let getPassword = getPasswordChecker('AKuvshinova_92');
 
-console.log(variantPassword1());
-console.log(variantPassword2());
-console.log(variantPassword3());
-console.log(variantPassword4());
+console.log(getPassword(('AKuvshinova_92')));
+console.log(getPassword(('AKuvshinova_92_')));
+console.log(getPassword(('%;%:;?№*%?::?%:%?:%')));
+console.log(getPassword(('AKUVSHINOVA_92')));
