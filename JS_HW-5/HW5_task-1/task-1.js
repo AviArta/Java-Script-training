f765979a9cfdb885b3b62855e6bf25781eb93e1c
AviArta <buzlova.a@yandex.ru>
2023-03-29 22:00:22 +0300

    Add spead

--- JS_HW-5/HW5_task-1/task-1.js
+++ JS_HW-5/HW5_task-1/task-1.js
@@ -1,20 +1,61 @@
-//const timerHh = document.getElementById('hours')
-//const timerMm = document.getElementById('minits')
+const timerHh = document.getElementById('hours')
+const timerMm = document.getElementById('minits')
 const timerSs = document.getElementById('seconds')
 
-setInterval(() => {
+function timer1 () {
+  setInterval(() => {
+    const timeH = timerHh.textContent;
+    if (timeH > 0) {
+    timerHh.textContent = '0' + Number(timerHh.textContent) - 1;
+    } else {
+      window.clearInterval(timeOut);
+    }
+  }, 5000);} // 3600000
+
+function timer2 () {
+  setInterval(() => {
+    const timeM = timerMm.textContent;
+    if (timeM > 0) {
+      timerMm.textContent = '0' + (Number(timerMm.textContent) - 1);
+    } else {
+      window.clearInterval(timeOut);
+    }
+  }, 2000); // 60000
+}
+
+function timer3 () {
+  setInterval(() => {
+    const timeS = timerSs.textContent;
+    if (timeS > 0) {
+      if (timeS < 10) {
+        timerSs.textContent = '0' + (Number(timerSs.textContent) - 1);
+      } else {
+        timerSs.textContent = Number(timerSs.textContent) - 1;
+      }
+    } else {
+      document.location.assign('https://netology.ru');
+      window.clearInterval(timeOut);
+    }
+  }, 500);
+} 
+
+function total() {
+  return timer1(), timer2(), timer3()
+}
+
+total();
+
+
+//setInterval(() => {
   //const timeH = timerHh.textContent;
   //const timeM = timerMm.textContent;
-  const timeS = timerSs.textContent;
+  //const timeS = timerSs.textContent;
 
-  if (timeS > 0) {
-  timerSs.textContent = Number(timerSs.textContent) - 1;
-  } else {
+  //if (timeS > 0) {
+  //timerSs.textContent = Number(timerSs.textContent) - 1;
+  //} else {
   //alert('Вы победили!');
-  document.location.assign('https://netology.ru');
-  window.clearInterval(timeOut);
-  }
-}, 1000)
-  
-
-// Запуталась с часами, требует доработки.
+  //document.location.assign('https://netology.ru');
+  //window.clearInterval(timeOut);
+  //}
+//}, 1000)
--- JS_HW-5/HW5_task-2/task-2.js
+++ JS_HW-5/HW5_task-2/task-2.js
@@ -4,13 +4,13 @@ const speed = document.getElementById('speed__counter')
 const sec1 = document.getElementById('sec1')
 const sec2 = document.getElementById('sec2')
 
-//const date1 = new Date;
+const date1 = new Date();
 //sec1.textContent = date1.getSeconds();
 
 cookie.onclick = () => {
-  const date1 = new Date;
+  //date1 = new Date;
   sec1.textContent = date1.getSeconds();
-  
+
   checkKlick.textContent = Number(checkKlick.textContent) + 1;
 
   if (checkKlick.textContent % 2 != 0) {
@@ -21,8 +21,10 @@ cookie.onclick = () => {
     cookie.height -=20;
   }
 
-  const date2 = new Date;
+  const date2 = new Date();
   sec2.textContent = date2.getSeconds();
-  speed.textContent = 1 / (Number(date2.getSeconds()) - Number(date2.getSeconds())); 
-  date1 = date2; //надо перезаписать начальные секунды на конечные после каждого клика.
+  speed.textContent = Math.abs((1 / (Number(sec2.textContent) - Number(sec1.textContent))).toFixed(2)); 
+  date1 = new Date();
+  sec1.textContent = date1.getSeconds();
+  //date1 = date2; //надо перезаписать начальные секунды на конечные после каждого клика.
 }
