cddd092b6f333abae0f09771e2a3a4988522a8be
AviArta <buzlova.a@yandex.ru>
2023-04-17 18:54:01 +0300

    hw7 task-2 difficult level

--- JS_HW-7/HW7_task-2/hw7_task-2.js
+++ JS_HW-7/HW7_task-2/hw7_task-2.js
@@ -1,4 +1,4 @@
-// Базовая версия задания.
+// Повыщенный уровень сложности (два меню).
 
 // Найти объект клика:
 document.onclick = function (event) {
@@ -7,8 +7,61 @@ document.onclick = function (event) {
     // Создать массивы из списков:
     let menuItems = document.querySelectorAll('.menu__link')
     let menuArr = Array.from(menuItems);
+    //console.log('menuArr:', menuArr);
+    //console.log('menuArr:', menuArr.slice([0], [9]));
+    //console.log('menuArr:', menuArr.slice([9], [18]));
 
-    if (obj.parentElement.querySelector('ul')) {
+    let ind = menuArr.indexOf(obj);
+    //console.log(ind);
+
+    //первое меню:
+    if (ind <= 8) {
+        console.log('Нажали на первое меню.');
+        if (obj.parentElement.querySelector('ul')) {
+     
+            menuArr.slice([0], [9]).forEach(element => {
+                
+                if (element.parentElement.querySelector('ul')) {
+                    element.parentElement.querySelector('ul').className = 'menu menu_sub';
+                }});
+                obj.parentElement.querySelector('ul').className = 'menu menu_sub menu_active';
+    
+            } else { 
+                // Все подменю возвращаем в неактивное состояние.
+                menuArr.slice([0], [9]).forEach(ele => {
+                    if (ele.parentElement.querySelector('ul')) {
+                        ele.parentElement.querySelector('ul').className = 'menu menu_sub';
+                        return false 
+            }});}
+    
+        return false // убрать потом, чтобы  ссылки в меню без подменю работали.
+
+    //второе меню:
+    } else {
+        console.log('Нажали на второе меню.');
+        if (obj.parentElement.querySelector('ul')) {
+     
+            menuArr.slice([9], [18]).slice([0], [9]).forEach(element => {
+                
+                if (element.parentElement.querySelector('ul')) {
+                    element.parentElement.querySelector('ul').className = 'menu menu_sub';
+                }});
+                obj.parentElement.querySelector('ul').className = 'menu menu_sub menu_active';
+    
+            } else { 
+                // Все подменю возвращаем в неактивное состояние.
+                menuArr.slice([9], [18]).forEach(ele => {
+                    if (ele.parentElement.querySelector('ul')) {
+                        ele.parentElement.querySelector('ul').className = 'menu menu_sub';
+                        return false 
+            }});}
+
+        return false // убрать потом, чтобы  ссылки в меню без подменю работали.
+    }
+}
+    
+    // Базовая версия задания.
+    /*if (obj.parentElement.querySelector('ul')) {
      
         menuArr.forEach(element => {
             
@@ -26,4 +79,4 @@ document.onclick = function (event) {
         }});}
 
     return false // убрать потом, чтобы  ссылки в меню без подменю работали.
-}
+}*/
