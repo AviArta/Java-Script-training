b71642b77137f4b29ee9feb2fe9b0887ab1c5192
AviArta <buzlova.a@yandex.ru>
2023-04-17 16:56:59 +0300

    hw7 task-3 simple version

--- JS_HW-7/HW7_task-3/hw7_task-3.js
+++ JS_HW-7/HW7_task-3/hw7_task-3.js
@@ -1,23 +1,51 @@
 function onClickPrev() {
     console.log('НАЗАД');
+    console.log('imgArr.length', imgArr.length);
+    //console.log('counter', counter);
+    if (counter > 0) {
+        console.log('Я тут.')
+        let activeImg = counter - 1;
+        console.log('counter =', counter, 'activeImg =', activeImg);
+        imgArr[counter].className = 'slider__item';
+        //console.log('counter =', counter, 'activeImg =', activeImg);
+        imgArr[activeImg].className = 'slider__item slider__item_active';
+        sliderArr[counter].className = 'slider__dot';
+        sliderArr[activeImg].className = 'slider__dot slider__dot_active';
+        counter--;
+    } else {
+        console.log('А теперь тут!!!!');
+        counter = imgArr.length;
+        activeImg = counter - 1;
+        imgArr[0].className = 'slider__item';
+        console.log('counter =', counter, 'activeImg =', activeImg);
+        imgArr[activeImg].className = 'slider__item slider__item_active';
+        sliderArr[0].className = 'slider__dot';
+        sliderArr[activeImg].className = 'slider__dot slider__dot_active';
+        counter--;
+    }
 }
 
 function onClickNext() {
     console.log('ВПЕРЁД');
-
-    imgArr.forEach(img => {
-        let ind1 = imgArr.indexOf(img);
-
-        if (ind1 < imgArr.length - 1) {
-            let ind2 = ind1 + 1;
-            imgArr[ind1].className = 'slider__item';
-            console.log('ind1 =', ind1, 'ind2 =', ind2);
-            imgArr[ind2].className = 'slider__item slider__item_active';
-            //console.log('imgArr[ind2] =', imgArr[ind2].className);
-            //ind1++;
-        } 
-        ind1++;
-    });
+    //console.log('imgArr.length', imgArr.length);
+    if (counter < imgArr.length - 1) {
+        let activeImg = counter + 1;
+        imgArr[counter].className = 'slider__item';
+        //console.log('counter =', counter, 'activeImg =', activeImg);
+        imgArr[activeImg].className = 'slider__item slider__item_active';
+        sliderArr[counter].className = 'slider__dot';
+        sliderArr[activeImg].className = 'slider__dot slider__dot_active';
+        counter++;
+    } else {
+        //console.log('counter=', counter);
+        activeImg = 0;
+        imgArr[counter].className = 'slider__item';
+        //console.log('counter =', counter, 'activeImg =', activeImg);
+        imgArr[activeImg].className = 'slider__item slider__item_active';
+        sliderArr[counter].className = 'slider__dot';
+        sliderArr[activeImg].className = 'slider__dot slider__dot_active';
+        counter = 0;
+    }    
 }
 
 let btnPrev = document.getElementsByClassName('slider__arrow slider__arrow_prev');
@@ -30,8 +58,61 @@ btnPrevArr.forEach(element1 => {
 
 let btnNextArr = Array.from(btnNext);
 btnNextArr.forEach(element2 => {
+    console.log('Вперёд__')
     element2.addEventListener('click', onClickNext);
     });
 
 let imgItems = document.querySelectorAll('.slider__item');
 let imgArr = Array.from(imgItems);
+
+let counter = 0;
+
+let sliders = document.querySelectorAll('.slider__dot');
+let sliderArr = Array.from(sliders);
+
+// !!! КОД РАБОТАЕТ ТОЛЬКО ОТДЕЛЬНО на стрелки или точки !!!
+
+/*document.onclick = function (event) {
+    let obj = event.target;
+    console.log('obj.className=', obj.className);
+
+    // если нажали на точки:
+    if (obj.className = 'slider__dot') {
+        obj.className = 'slider__dot slider__dot_active';
+
+        // Определить индекс элемента, на который кликнули, в созданном массиве:
+        let ind = sliderArr.indexOf(obj);
+        
+        if (ind >= 0) {
+            console.log('Нажали слайдер', ind + 1);
+            imgArr[ind].className = 'slider__item slider__item_active';
+    
+        //Поставить остальные элементы в первоначальное состояние:
+            imgArr.forEach(element => {
+                if (imgArr.indexOf(element) != ind) {
+                    //console.log('imgArr[imgArr.indexOf(element)] =', imgArr.indexOf(element));
+                    imgArr[imgArr.indexOf(element)].className = 'slider__item';
+                    sliderArr[imgArr.indexOf(element)].className = 'slider__dot';
+                }
+            });
+        } 
+    
+    // !!! ЭТА ЧАСТЬ НЕ РАБОТАЕТ !!!
+    // если нажали стрелки:
+    /*} else if (obj.className == 'slider__arrow') {
+        console.log('Нажали за границей слайдера и ВПЕРЁД/НАЗАД.');
+        console.log('obj.className =', obj.className);
+        if (obj.className == 'slider__arrow slider__arrow_next') {
+            btnNextArr.forEach(element2 => {
+                console.log('Вперёд__')
+                element2.addEventListener('click', onClickNext);
+                });
+        } else if (obj.className == 'slider__arrow slider__arrow_prev') {
+            btnPrevArr.forEach(element1 => {
+                element1.addEventListener('click', onClickPrev);
+                });
+        }
+    } else {
+        console.log('Нажали за границей слайдера и кнопкок ВПЕРЁД/НАЗАД.');
+    }    
+}*/
