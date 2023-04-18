3b9fb22b97ce114604c7ad1b358b86e90343bc79
AviArta <buzlova.a@yandex.ru>
2023-04-18 08:03:10 +0300

    hw6 task-2

--- JS_HW-6/HW6_task-1/hw6_task-1.js
+++ JS_HW-6/HW6_task-1/hw6_task-1.js
@@ -1,52 +1,58 @@
 function onClick1() {
-    console.log('Кнопка нажата');
-
+    console.log('Главная кнопка нажата');
     // При нажатии раскрыть весь список:
-    for (var i=0; i < dropdownList.length; i++) {
-        dropdownList[i].className = 'dropdown__list dropdown__list_active';
-    }
+    listArr[0].className = 'dropdown__list dropdown__list_active';
+    return false
 }
 
-function onClick2() {
+/*function onClick2() {
     console.log('Выбор сделан');
+    bottonArr[0].textContent = '';
+    listArr[0].className = 'dropdown__list';
     //console.log('arrDropdown:', arrDropdown);
-    console.log('botton[i].textContent=', botton[i].textContent, 'dropdown[j].textContent=', dropdown[j].textContent);
-    botton[i].textContent = dropdown[j].textContent;
+    return false
+}*/
 
-    let item = document
-    //document.onclick = function (event) {
-        //let obj = event.target;
-        //console.log('obj.textContent=', obj.textContent);
-    
-        // Создать массивы из списков:
-        //let arrDropdown = Array.from(dropdown);
-  
-     //}
-}
+let botton = document.getElementsByClassName('dropdown__value');
+let bottonArr = Array.from(botton);
+
+let dropdownList = document.getElementsByClassName('dropdown__list'); //+
+let listArr = Array.from(dropdownList);
+
+let dropdown = document.querySelectorAll('.dropdown__link');
+let dropItemArr = Array.from(dropdown);
+
+console.log(bottonArr[0].textContent);
 
 // Обработка первого клика:
-const botton = document.getElementsByClassName('dropdown__value');
-const dropdownList = document.getElementsByClassName('dropdown__list');
-const dropdown = document.getElementsByClassName('dropdown__item')
-
-for (var i=0; i < botton.length; i++) {
-    botton[i].addEventListener('click', onClick1);
-    console.log('botton[i].textContent:', botton[i].textContent);
-}
-
-for (var i=0; i < dropdownList.length; i++) {
-    dropdownList[i].addEventListener('click', onClick1);
-}
+bottonArr.forEach(element1 => {
+    element1.addEventListener('click', onClick1);
+    });
 
 // Обработка второго клика (выбора);
-for (var j=0; j < dropdown.length; j++) {
-    dropdown[j].addEventListener('click', (evn) => {
-        console.log('Выбор сделан');
-        console.log('evn.target.textContent=', evn.target.textContent);
-        botton[0].textContent = evn.target.textContent;
-    }
-    );
+document.onclick = function (event) {
+    let obj = event.target;
+    //let ind = dropItemArr.indexOf(obj);
+    console.log(dropItemArr);
+    dropItemArr.forEach(element => {
+        console.log('closest=', element.className, element.textContent);
+        });
+    //listArr[0].className = 'dropdown__list';
+    //bottonArr[0].textContent = dropItemArr[ind].textContent;
+    console.log('bottonArr[0].textContent=', bottonArr[0].textContent);
+    return false
 }
 
-
-
+/*for (var j=0; j < dropItemArr.length; j++) {
+    dropItemArr[j].addEventListener('click', (evn) => {
+        console.log('Выбор сделан');
+        console.log('evn.target.textContent=', evn.target.textContent);
+        bottonArr[0].textContent = evn.target.textContent;
+        console.log('!!!!!!!', evn.target.textContent);
+        // Закрыть список
+        listArr[0].className = 'dropdown__list';
+        console.log(bottonArr[0].textContent);
+        return false
+    }
+    );
+}*/
